e3fdfa738e9ac29859f9333ce56c2bb08e596647
isaiasmezaclearclear <isaiasmeza>
2020-11-29 22:30:53 -0800

    mario_pest_control

---
+++ mario.js
@@ -0,0 +1,16 @@
+function totals(){
+    
+    var gomB = document.getElementById("Goombas").value;
+    var bob = document.getElementById("Bob-ombs").value;
+    var cheep = document.getElementById("Cheep-Cheeps").value;
+    var gomBPest = parseInt(gomB, 10);
+    var bobPest = parseInt(bob, 10);
+    var cheepPest = parseInt(cheep, 10);
+    var totalPestCaught = gomBPest + bobPest + cheepPest; 
+    var totalCost = (gomBPest * 5) + (bobPest * 7) + (cheepPest * 11);
+    var x = document.getElementById("totalPest");
+    var m = document.createTextNode("Total Pest Caught is:" + " " + totalPestCaught + "..... " + "Total Cost of The Service is:" + " " + totalCost);
+    x.appendChild(m);
+}
+    
+
